b1345674cbe6f0e869fd337efd58b53e4b42de43
Nahiara <nahiaralucia@hotmail.com>
2025-08-20 16:31:26 -0300

    Agrego mas puntos

--- TS1.py
+++ TS1.py
@@ -16,13 +16,45 @@ def mi_funcion_sen( vmax, dc, ff, ph, nn, fs):
 
     return tt,xx 
 
-N=1000 
-fs=1000
+N=200 #bajo las muestras para que se vea mejor la senoidal)
+fs=20000 #debo poner al menos el doble de la frecuencia que quiero esstudiar 
+#use 2kHz asi que al menos mi frec de muestreo deeria ser 4kHz pero puse 20k para darme mas margen
 tt, yy = mi_funcion_sen(1, 0, 2000, 0, N, fs)
 plt.figure(1)
+plt.subplot(1,2,1)
 plt.plot(tt, yy, color='orchid') 
 plt.title('Señal Senoidal')
 plt.xlabel('Tiempo [s]')
 plt.ylabel('Amplitud [Volts]')
 plt.grid(True)
+plt.show()
+
+#SEÑAL AMPLIFICADA Y DESFASADA PI/2 (de amplitud 1 a 5)
+tt1, yy1 = mi_funcion_sen(6, 0, 2000, np.pi/2, N, fs)
+plt.subplot(1,2,2)
+plt.plot(tt1, yy1, color='rebeccapurple') 
+plt.title('Señal Senoidal')
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud [Volts]')
+plt.grid(True)
+plt.show()
+
+plt.tight_layout()  
+plt.show()
+
+
+#SEÑAL MODULADA
+fsm=20000
+Tsm = 1/fsm #período de muestre (osea cuanto tiempo hay entre muestra y muestra)
+ttm = np.linspace(0, (N-1)*Tsm, N)
+xx3=1*np.sin(2*np.pi*2000*ttm)
+xxm=xx3*np.sin(2*np.pi*1000*ttm+np.pi/2)
+plt.figure(2)
+plt.plot(ttm, xxm, color='orchid') 
+plt.title('Señal Senoidal')
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud [Volts]')
+plt.grid(True)
+
+plt.tight_layout()  
 plt.show()
