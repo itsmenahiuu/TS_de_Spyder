3f36a90342bbae3dcc17e016c7689b3dd674b692
Nahiara <nahiaralucia@hotmail.com>
2025-10-11 21:42:03 -0300

    j

--- TS5.py
+++ TS5.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import signal as sig
 import matplotlib.pyplot as plt
+import scipy.io as sio
 from scipy.io import wavfile
 
 #--------------Para encontrar mi banda ancha--------------#
@@ -176,3 +177,99 @@ print(f"BW con 95%: {f95:.2f} Hz")
 print(f"BW con 97%: {f97:.2f} Hz")
 print(f"BW con 99%: {f99:.2f} Hz")
 print("=====================================")
+
+# %%
+
+#BONUS
+
+##################
+## ECG con ruido
+##################
+
+# para listar las variables que hay en el archivo
+mat_struct = sio.loadmat('./ECG_TP4.mat')
+
+ecg_ruido = mat_struct['ecg_lead'].flatten()  # señal con ruido (1D)
+N_ecg_ruido = len(ecg_ruido)
+
+hb_1 = mat_struct['heartbeat_pattern1']
+hb_2 = mat_struct['heartbeat_pattern2']
+
+plt.figure(7)
+plt.plot(ecg_ruido, color='mediumvioletred')
+plt.title('ECG con ruido')
+plt.xlabel('Muestras [n]')
+plt.ylabel('Amplitud [V]')
+plt.grid()
+plt.show()
+
+# Parámetros (idénticos a los del ECG sin ruido)
+nperseg = N_ecg_ruido // 30
+window = 'blackman'
+nfft = 6 * nperseg
+
+# Welch
+freqW_r, ecgW_r = sig.welch(ecg_ruido, fs=fs_ecg, window=window, nperseg=nperseg, nfft=nfft)
+
+# Periodograma
+freqP_r, ecgP_r = sig.periodogram(ecg_ruido, fs=fs_ecg, window=window, nfft=nfft)
+
+# Blackman–Tukey
+corr_r = sig.correlate(ecg_ruido - np.mean(ecg_ruido), ecg_ruido - np.mean(ecg_ruido), mode='full') / N_ecg_ruido
+corr_r = corr_r[corr_r.size // 2:]
+win_bt = sig.windows.bartlett(len(corr_r))
+corr_win_r = corr_r * win_bt
+PSD_bt_r = np.abs(np.fft.rfft(corr_win_r, n=nfft))
+freqBT_r = np.fft.rfftfreq(nfft, d=1/fs_ecg)
+
+# ---- Gráfico ----
+plt.figure(8)
+plt.plot(freqP_r, 10*np.log10(ecgP_r + eps), label='Periodograma', color="palevioletred")
+plt.plot(freqBT_r, 10*np.log10(PSD_bt_r + eps), label='Blackman-Tukey', color="limegreen")
+plt.plot(freqW_r, 10*np.log10(ecgW_r + eps), label='Welch', color="cornflowerblue")
+plt.title('ECG con ruido')
+plt.xlabel('Frecuencia [Hz]')
+plt.ylabel('PSD [dB]')
+plt.xlim([0, 50])
+plt.grid()
+plt.legend()
+plt.show()
+
+# ---- Potencia total y ancho de banda ----
+df_r = freqW_r[1] - freqW_r[0]
+potTotal_r = np.sum(ecgW_r) * df_r
+f95_r = estimar_BW(ecgW_r, freqW_r, 0.95)
+f97_r = estimar_BW(ecgW_r, freqW_r, 0.97)
+f99_r = estimar_BW(ecgW_r, freqW_r, 0.99)
+
+print("=====================================")
+print("ECG CON RUIDO")
+print(f"Potencia total (Welch): {potTotal_r:.4e} V^2")
+print(f"BW con 95%: {f95_r:.2f} Hz")
+print(f"BW con 97%: {f97_r:.2f} Hz")
+print(f"BW con 99%: {f99_r:.2f} Hz")
+print("=====================================")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
